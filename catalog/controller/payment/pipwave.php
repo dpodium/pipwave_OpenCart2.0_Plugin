36deca2a33dd7d4a931cb0acfb9e709e3a2b8c93
June <June@172.30.10.186>
2016-08-04 15:32:49 +0800

    Fix pipwave API `item_info` value structure.

--- catalog/controller/payment/pipwave.php
+++ catalog/controller/payment/pipwave.php
@@ -94,12 +94,14 @@ class ControllerPaymentPipwave extends Controller {
         $products = $this->cart->getProducts();
         foreach ($products as $key => $product) {
             $product_info = $this->model_catalog_product->getProduct($product['product_id']);
-            $data["item_info.{$key}.name"] = $product['name'];
-            $data["item_info.{$key}.description"] = $product['name'] . ' - ' . $product['model'];
-            $data["item_info.{$key}.amount"] = $this->currency->format($product['price'], $order_info['currency_code'], $order_info['currency_value'], false);
-            $data["item_info.{$key}.currency_code"] = $order_info['currency_code'];
-            $data["item_info.{$key}.quantity"] = $product['quantity'];
-            $data["item_info.{$key}.sku"] = $product_info['sku'];
+            $data['item_info'][] = array(
+                "name" => $product['name'],
+                "description" => $product['name'] . ' - ' . $product['model'],
+                "amount" => $this->currency->format($product['price'], $order_info['currency_code'], $order_info['currency_value'], false),
+                "currency_code" => $order_info['currency_code'],
+                "quantity" => $product['quantity'],
+                "sku" => $product_info['sku']
+            );
         }
 
         $pipwave_res = $this->_sendRequest($data);
